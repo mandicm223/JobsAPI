3200ac4319d162318ab2d41bdbfdb7e21d9fc714
JaSamLudiMoskri <78329923+JaSamLudiMoskri@users.noreply.github.com>
2021-11-22 16:06:07 +0100

    added swageer docs

--- app.js
+++ app.js
@@ -8,19 +8,21 @@ const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
 // Swagger
-//const swaggerUI = require('swagger-ui-express');
-//const YAML = require('yamljs');
-//const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
+
 // routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 // error handler
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -39,7 +41,7 @@ app.use(xss());
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
-//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
